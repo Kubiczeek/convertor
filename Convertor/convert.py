ec6a341c89b0a8929c030b7f79235d0a59c6a098
Kubiczeek <79270879+Kubiczeek@users.noreply.github.com>
2022-10-12 16:04:06 +0200

    Add files via upload

--- Convertor/convert.py
+++ Convertor/convert.py
@@ -1,6 +1,13 @@
+from tabulate import tabulate
+
+
 userInputNumber = input('Input number: ')
-userInputFormatInput = int(input('What is inputted number?(binary - 1; decimal - 2; hexadecimal - 3) '))
-userInputFormatOutput = int(input('What is the outputted number?(binary - 1; decimal - 2; hexadecimal - 3) '))
+userInputFormatInput = int(
+    input('What is inputted number?(binary - 1; decimal - 2; hexadecimal - 3) '))
+userInputFormatOutput = int(input(
+    'What is the outputted number?(binary - 1; decimal - 2; hexadecimal - 3) '))
+
+
 def checkIfPossible(number, choice):
 	if choice == 1:
 		try:
@@ -8,7 +15,7 @@ def checkIfPossible(number, choice):
 		except ValueError:
 			print("Inputted number isn't number")
 		except:
-			print("Something went wrong") 
+			print("Something went wrong")
 		possible = '10'
 		found = False
 		for n in number:
@@ -19,36 +26,66 @@ def checkIfPossible(number, choice):
 				continue
 		if found == True:
 			print("Inputted number isn't binary!")
+			return False
+		return True
 	elif choice == 2:
 		try:
 			int(number, 10)
 		except ValueError:
 			print("Inputted number isn't decimal! (or just don't use decimal point pls)")
+			return False
 		except:
-			print("Something went wrong") 
+			print("Something went wrong")
+		return True
 	elif choice == 3:
 		try:
 			int(number, 16)
 		except ValueError:
 			print("Inputted number isn't hexadecimal!")
+			return False
 		except:
 			print("Something went wrong")
+		return True
 	else:
-		return print('Error in format, please check it if you have inputted it right.')
+		print('Error in format, please check it if you have inputted it right.')
+		return False
+
 
 def binaryToDecimal(num):
 	listt = [int(i) for i in str(num)]
 	listt.reverse()
 	finalNumber = 0
 	finalString = ""
+	finalString2 = ""
 	for i in range(len(listt)):
 		finalNumber += listt[i]*(2**i)
-		finalString += str(listt[i]) + str("*2") + "^" + str(i) + "+"
+		finalString += str(listt[i])+"*"+str(2**i)+" + "
+		finalString2 += str(listt[i]*(2**i)) + " + "
 
 	print("\n\nResult:")
 	print(finalNumber)
 	print("\nMethod:")
-	print(finalString[:-1])
+	print("1)  "+finalString[:-2] + "	- We'll be multiplying the numbers now.")
+	print("2)  "+finalString2[:-2] + "			- Now, just the addition left.")
+	print("3)  "+str(finalNumber))
+
+
+def decimalToBinary(num):
+	changingNum = int(num)
+	endList = []
+	endList2 = [['Division by 2', 'Quotient', 'Reminder']]
+	while changingNum != 0:
+		reminder = changingNum % 2
+		changingNum = int((changingNum-reminder)/2)
+		endList.insert(0, str(reminder))
+		tempList = [str((changingNum-reminder)*2)+"/2", changingNum, reminder]
+		endList2.append(tempList)
+	
+	print("\n\nResult:")
+	print("".join(endList))
+	print("\nMethod:")
+	print(tabulate(endList2, headers='firstrow', tablefmt='fancy_grid'))
+	
 
 
 def Solve(number, choice, output):
@@ -56,17 +93,17 @@ def Solve(number, choice, output):
 		if output == 2:
 			binaryToDecimal(number)
 		elif output == 3:
-			print("xd")
+			print("d")
 	elif choice == 2:
-		int(number, 10)
+		if output == 1:
+			decimalToBinary(number)
+		elif output == 3:
+			print("d")
 	elif choice == 3:
 		int(number, 16)
 	
-
 if userInputFormatInput == userInputFormatOutput:
 	print('Input format cannot be the same as output')
 else:
-	checkIfPossible(userInputNumber, userInputFormatInput)
-	Solve(userInputNumber, userInputFormatInput, userInputFormatOutput)
-
-
+	if checkIfPossible(userInputNumber, userInputFormatInput) == True:
+		Solve(userInputNumber, userInputFormatInput, userInputFormatOutput)
